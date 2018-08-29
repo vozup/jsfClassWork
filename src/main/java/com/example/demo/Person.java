14f4de1b298b2f55d9323e7fcd23157670bd9799
vozup <vozupserega@gmail.com>
2018-08-29 19:25:08 +0300

    main

---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Person {
+    private String fName;
+    private String lName;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+}
